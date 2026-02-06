af6bc6ed0ed03de4d8177b63688300bd5a704e20
safaacht <safaachtaoui@gmail.com>
2026-02-06 12:50:02 +0100

    insertion d'admin en seeder

---
+++ GluFree/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         User::insert([
+            'name'=>'Safaa',
+            'email'=>'chtaouisafaa@gmail.com',
+            'password'=>Hash::make('chtaouisafaa@gmail.com'),
+            'role'=>'admin'
+        ]);
+    }
+}
